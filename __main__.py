77066311038e0e39559cea7994b760f7a685e25f
Anton Pryhodko <anton_pryhodko@epam.com>
2022-06-17 17:41:36 +0300

    Create Service

--- __main__.py
+++ __main__.py
@@ -1,6 +1,10 @@
 import pulumi
 import pulumi_aws as aws
 
+AZ = "us-east-1a"
+
+ServiceSubnet = aws.ec2.get_subnet(availability_zone=AZ) 
+
 GameServersCluster = aws.ecs.Cluster ("GameServer", settings=[aws.ecs.ClusterSettingArgs(
 	name="containerInsights", 
 	value="enabled")])
@@ -14,8 +18,15 @@ Minecraft = aws.ecs.TaskDefinition("MinecraftBedrock",
 	memory="2048")
 
 
+MinecraftService = aws.ecs.Service("MinecraftBedrockService",
+	cluster = GameServersCluster,
+	task_definition = Minecraft,
+	desired_count = 1,
+	launch_type = "FARGATE",
+	network_configuration = aws.ecs.ServiceNetworkConfigurationArgs(
+		subnets = [ServiceSubnet.id],
+		assign_public_ip = True))
 
 
-
-# pulumi.export("output", GameServersCluster)
+pulumi.export("output", ServiceSubnet)
 

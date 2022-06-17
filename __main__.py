a6ab4d5da2541c411b120c6e7b777855606eb627
Anton Pryhodko <anton_pryhodko@epam.com>
2022-06-17 16:54:22 +0300

    Create ECS cluster

--- __main__.py
+++ __main__.py
@@ -1,10 +1,9 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+GameServersCluster = aws.ecs.Cluster ("GameServer", settings=[aws.ecs.ClusterSettingArgs(
+	name="containerInsights", 
+	value="enabled")])
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+pulumi.export("output", GameServersCluster)
+
